7c3ff9ae601f82c7c176f4c955e370663c10c901
Kolovratik <Kolovratik@HOME-PC>
2024-10-05 18:08:01 +0300

    Разработанные переменные

--- app/src/main/java/com/example/gps/MainActivity.java
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.gps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    TextView tvResult;
+    int ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvResult = findViewById(R.id.tvResult);
     }
+
+    LocationListener _LocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if(location == null) return;
+            else{
+                String message = "";
+                if(location.getProvider().equals(LocationManager.GPS_PROVIDER)){
+                    message += "Местоположение определено с помощью GPS: долгота - " + location.getLatitude() + " широта - " + location.getLongitude();
+                }
+                if(location.getProvider().equals(LocationManager.NETWORK_PROVIDER)){
+                    message += "Местоположение определено с помощью интернета: долгота - " + location.getLatitude() + " широта - " + location.getLongitude();
+                }
+                tvResult.setText(message);
+            }
+        }
+    };
 }
